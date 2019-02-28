5aa08dbe374835b0f70edebc2b911a148a390e75
Keith Morgan <kthmorgan@gmail.com>
2019-02-28 09:53:41 -0600

    added comment for running pihole -g after adding the white list

--- addwhitelist.sh
+++ addwhitelist.sh
@@ -2,4 +2,5 @@ DBS=`cat whitelist.txt` #Read in file of ip addresses
 for b in $DBS ; do # enter a loop to act on each ip
 pihole -w -nr "$b"
 done 
+# Dont forget to run pihole -g to restart gravity
 
